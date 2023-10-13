5d5a3e2a6c054501a028a7f3be7b2dfc14040122
KamiFightingSpirit <55668557+KamiFightingSpirit@users.noreply.github.com>
2023-10-13 12:20:54 -0700

    Optimization

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-const robot =  require("robotjs");
+const robot = require("robotjs");
 let ropeHookCount = 0;
 let mouseClickCount = 0;
-console.log("Starting Program...")
+console.log("Starting Program...");
 setInterval(performClick, 400);
 
 // Function to perform a click
 function performClick() {
   mouseClickCount++;
+  if (mouseClickCount % 10 === 0) console.log(mouseClickCount);
   robot.mouseClick();
   //average firefly respawn time can be found in settings
-  if(mouseClickCount % 140 === 0) { 
-    const currentPosition = robot.getMousePos();
-    robot.moveMouse(currentPosition.x + 50, currentPosition.y);
-    robot.mouseClick();
-    ropeHookCount++;
-    robot.moveMouse(currentPosition.x, currentPosition.y);
-  }
+  if (mouseClickCount % 140 === 0) throwShuriken();
+}
+
+function throwShuriken() {
+  const currentPosition = robot.getMousePos();
+  robot.moveMouse(currentPosition.x + 50, currentPosition.y);
+  robot.mouseClick();
+  console.log("Hadooooken!");
+  ropeHookCount++;
+  robot.moveMouse(currentPosition.x, currentPosition.y);
 }
 
 // Function to simulate pressing the "q" key
@@ -31,9 +35,11 @@ function scheduleKeyPress() {
   setTimeout(scheduleKeyPress, 8000);
 }
 
-process.on('SIGINT', () => {
-  console.log(`This program performed ${ropeHookCount} ropehooks and ${mouseClickCount} mouseclicks for you!`)
-  console.log('Exiting program');
+process.on("SIGINT", () => {
+  console.log(
+    `This program performed ${ropeHookCount} shurikens and ${mouseClickCount} mouseclicks for you!`
+  );
+  console.log("Exiting program");
 
   // You can perform any cleanup or additional actions here if needed
   process.exit(0); // Exit the program gracefully
