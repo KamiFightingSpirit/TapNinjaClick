1b7d66033d467c8c6f326b10415edd5fc84f1884
KamiFightingSpirit <55668557+KamiFightingSpirit@users.noreply.github.com>
2023-10-27 14:13:32 -0400

    Refactoring

--- index.js
+++ index.js
@@ -1,46 +1,24 @@
 const robot = require("robotjs");
+const throwShuriken = require("./lib/actions/throwShuriken");
+const mouseClick = require("./lib/actions/mouseClick");
+
 let ropeHookCount = 0;
 let mouseClickCount = 0;
-console.log("Starting Program...");
-setInterval(performClick, 400);
+let intervalId;
+let programRunning = false;
 
-// Function to perform a click
-function performClick() {
-  mouseClickCount++;
-  if (mouseClickCount % 10 === 0) console.log(mouseClickCount);
-  robot.mouseClick();
+function startInterval() {
+  // let message = programRunning ? "Stopping Program" : "Starting Program...";
+  // console.log(message);
+  return setInterval(runner, 400);
+}
+startInterval();
+
+function runner() {
+  //perform a click
+  mouseClickCount = mouseClick(mouseClickCount);
   //average firefly respawn time can be found in settings
-  if (mouseClickCount % 140 === 0) throwShuriken();
+  if (mouseClickCount % 4 === 0) {
+    ropeHookCount = throwShuriken(ropeHookCount);
+  }
 }
-
-function throwShuriken() {
-  const currentPosition = robot.getMousePos();
-  robot.moveMouse(currentPosition.x + 50, currentPosition.y);
-  robot.mouseClick();
-  console.log("Hadooooken!");
-  ropeHookCount++;
-  robot.moveMouse(currentPosition.x, currentPosition.y);
-}
-
-// Function to simulate pressing the "q" key
-function simulateKeyPress() {
-  robot.keyTap("q");
-}
-
-// Function to schedule the keypress every 20 seconds
-function scheduleKeyPress() {
-  simulateKeyPress(); // Simulate the keypress immediately
-
-  // Schedule the next keypress after 20 seconds (20000 milliseconds)
-  setTimeout(scheduleKeyPress, 8000);
-}
-
-process.on("SIGINT", () => {
-  console.log(
-    `This program performed ${ropeHookCount} shurikens and ${mouseClickCount} mouseclicks for you!`
-  );
-  console.log("Exiting program");
-
-  // You can perform any cleanup or additional actions here if needed
-  process.exit(0); // Exit the program gracefully
-});
